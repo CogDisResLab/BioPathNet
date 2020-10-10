1bbf5b2a37f9d2d784a44ad9892b1c7adfe22925
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-10-10 17:33:15 -0400

    Added the get_revigo_output function for Revigo enrichment

---
+++ R/get_revigo_output.R
@@ -0,0 +1,34 @@
+#' Perform Revigo enrichment analysis
+#'
+#' @param input_data A dataframe containing GO IDs and associated p-values
+#'
+#' @return A dataframe containing the Revigo Enrichment analysis
+#' @export
+#'
+#' @examples
+#'
+get_revigo_output <- function(input_data) {
+  java_binary <- system.file("java", "RevigoStandalone.jar", package = "BioPathNet")
+
+  temp <- tempfile()
+  input_data %>%
+    readr::write_tsv(temp, col_names = F)
+
+  command <- stringr::str_glue("java -jar {java_binary} {temp} --stdout")
+
+  output <- system(command, intern = T)
+
+  output_df <- readr::read_tsv(
+    stringr::str_c(output, collapse = "\n"),
+    skip = 2
+  ) %>%
+    dplyr::rename(namespace = `%namespace`) %>%
+    dplyr::select(namespace, term_ID, description,
+                  frequency, `log10_p-value`, uniqueness,
+                  dispensability, representative) %>%
+    dplyr::rename_with(stringr::str_to_title) %>%
+    dplyr::rename(GOID = Term_id,
+                  Log10_pval = `Log10_p-Value`)
+
+  return(output_df)
+}
---
+++ tests/testthat/test-get_revigo_output.R
@@ -0,0 +1,3 @@
+test_that("revigo enrichment works", {
+  expect_true(TRUE)
+})
