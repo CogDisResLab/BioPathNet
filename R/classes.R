7564ecf11cbe052ea78c55f89d04fe39ba9cbd3e
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-10-11 16:20:04 -0400

    Updated the class documentation

--- R/classes.R
+++ R/classes.R
@@ -1,46 +1,122 @@
 require(methods)
 
+#' GSEAResult-class: Class storing GSEA Results
+#'
+#' This class stores the input parameters and results of the Gene Set Enrichment Analysis.
+#'
+#' @slot alpha numeric. The Alpha threshold for significance
+#' @slot threshold_up numeric. The logfc threshold for up-regulated genes
+#' @slot threshold_down numeric. The logfc threshold for down-regulated genes
+#' @slot upregulated_genes tbl_df. The up-regulated genes from the input DEG tibble
+#' @slot downregulated_genes tbl_df. The down-regulated genes from the input DEG tibble
+#' @slot pos_enriched tbl_df. The positively enriched pathways
+#' @slot neg_enriched tbl_df. The negatively enriched pathways
+#' @slot sig_pos_enriched tbl_df. Significant positively enriched pathways
+#' @slot sig_neg_enriched tbl_df. Significant positively enriched pathways
+#' @slot num_upreg numeric. Number of up-regulated genes
+#' @slot num_downreg numeric. Number of down-regulated genes
+#' @slot num_tested numeric. Number of pathways tested
+#' @slot num_pos_enriched numeric. Number of positively enriched pathways
+#' @slot num_neg_enriched numeric. Number of negatively enriched pathways
+#' @slot num_sig_pos_enriched numeric. Number of significant positively enriched pathways
+#' @slot num_sig_neg_enriched numeric. Number of significant negatively enriched pathways
+#' @slot pathways tbl_df. The pathways we used, either built-in or user-supplied
+#'
+#' @return
+#' @export
+#'
+#' @examples
 setClass("GSEAResult",
          slots = c(
-           alpha_gsea = "numeric", # The Alpha threshold for significance
-           threshold_up_gsea = "numeric", # The logfc threshold for up-regulated genes
-           threshold_down_gsea = "numeric", # the logfc threshold for down-regulated genes
-           upregulated_gsea = "tbl_df", # The up-regulated genes from the input DEG tibble
-           downregulated_gsea = "tbl_df", # The down-regulated genes from the input DEG tibble
+           alpha = "numeric", # The Alpha threshold for significance
+           threshold_up = "numeric", # The logfc threshold for up-regulated genes
+           threshold_down = "numeric", # the logfc threshold for down-regulated genes
+           upregulated_genes = "tbl_df", # The up-regulated genes from the input DEG tibble
+           downregulated_genes = "tbl_df", # The down-regulated genes from the input DEG tibble
            pathways = "tbl_df", # The pathways we used, either built-in or user-supplied
-           pos_gsea_enriched = "tbl_df", # The positively enriched pathways
-           neg__gsea_enriched = "tbl_df", # The negatively enriched pathways
-           sig_pos_gsea_enriched = "tbl_df", # Significant positively enriched pathways
-           sig_neg_gsea_enriched = "tbl_df", # Significantly negatively enriched pathways
-           num_upreg_gsea = "numeric", # Number of up-regulated genes
-           num_downreg_gsea = "numeric", # Number of down-regulated genes
-           num_tested_gsea = "numeric", # Number of pathways tested
-           num_pos_enriched_gsea = "numeric", # Number of positively enriched pathways
-           num_neg_enriched_gsea = "numeric", # Number of negatively enriched pathways
-           num_sig_pos_enriched_gsea = "numeric", # Number of significant positively enriched pathways
-           num_sig_neg_enriched_gsea = "numeric" # Number of significant negatively enriched pathways
+           pos_enriched = "tbl_df", # The positively enriched pathways
+           neg_enriched = "tbl_df", # The negatively enriched pathways
+           sig_pos_enriched = "tbl_df", # Significant positively enriched pathways
+           sig_neg_enriched = "tbl_df", # Significantly negatively enriched pathways
+           num_upreg = "numeric", # Number of up-regulated genes
+           num_downreg = "numeric", # Number of down-regulated genes
+           num_tested = "numeric", # Number of pathways tested
+           num_pos_enriched = "numeric", # Number of positively enriched pathways
+           num_neg_enriched = "numeric", # Number of negatively enriched pathways
+           num_sig_pos_enriched = "numeric", # Number of significant positively enriched pathways
+           num_sig_neg_enriched = "numeric" # Number of significant negatively enriched pathways
          ))
 
+
+#' EnrichRResult-class: Class storing EnrichR Results
+#'
+#' This class stores the input parameters and results of the Gene Set Enrichment Analysis.
+#'
+#' @slot alpha numeric. The Alpha threshold for significance
+#' @slot threshold_up numeric. The logfc threshold for up-regulated genes
+#' @slot threshold_down numeric. The logfc threshold for down-regulated genes
+#' @slot upregulated_genes tbl_df. The up-regulated genes from the input DEG tibble
+#' @slot downregulated_genes tbl_df. The down-regulated genes from the input DEG tibble
+#' @slot dbs_used character. A vector of the databases used from enrichR
+#' @slot up_enrichr tbl_df. Enriched pathways in up-regulated genes
+#' @slot down_enrichr tbl_df. Enriched pathways in down-regulated genes
+#' @slot sig_up_enrichr tbl_df. Significant enriched pathways in up-regulated genes
+#' @slot sig_down_enrichr tbl_df. Significant enriched pathways in down-regulated genes
+#' @slot num_upreg numeric. Number of up-regulated genes
+#' @slot num_downreg numeric. Number of down-regulated genes
+#' @slot num_up_enrichr numeric. Number of enriched pathways in up-regulated genes
+#' @slot num_down_enrichr numeric. Number of enriched pathways in down-regulated genes
+#' @slot num_sig_up_enrichr numeric. Number of significant enriched pathways in up-regulated genes
+#' @slot num_sig_down_enrichr numeric. Number of significant enriched pathways in down-regulated genes
+#'
+#' @return
+#' @export
+#'
+#' @examples
 setClass("EnrichRResult",
          slots = c(
-           alpha_enrichr = "numeric", # The Alpha threshold for significance
-           threshold_up_enrichr = "numeric", # The logfc threshold for up-regulated genes
-           threshold_down_enrichr = "numeric", # the logfc threshold for down-regulated genes
-           upregulated_enrichr = "tbl_df", # The up-regulated genes from the input DEG tibble
-           downregulated_enrichr = "tbl_df", # The down-regulated genes from the input DEG tibble
+           alpha = "numeric", # The Alpha threshold for significance
+           threshold_up = "numeric", # The logfc threshold for up-regulated genes
+           threshold_down = "numeric", # the logfc threshold for down-regulated genes
+           upregulated_genes = "tbl_df", # The up-regulated genes from the input DEG tibble
+           downregulated_genes = "tbl_df", # The down-regulated genes from the input DEG tibble
            dbs_used = "character", # The names of the databases used to generate enrichment scores
-           up_enrichr_enriched = "tbl_df", # Enriched pathways in up-regulated genes
-           down_enrichr_enriched = "tbl_df", # Enriched pathways in down-regulated genes
-           sig_up_enrichr_enriched = "tbl_df", # Significant enriched pathways in up-regulated genes
-           sig_down_enrichr_enriched = "tbl_df", # Significant enriched pathways in down-regulated genes
-           num_upreg_enrichr = "numeric", # Number of up-regulated genes
-           num_downreg_enrichr = "numeric", # Number of down-regulated genes
-           num_up_enrichr_enriched = "numeric", # Number of enriched pathways in up-regulated genes
-           num_down_enrichr_enriched = "numeric", # Number of enriched pathways in down-regulated genes
-           num_sig_up_enrichr_enriched = "numeric", # Number of significant enriched pathways in up-regulated genes
-           num_sig_down_enrichr_enriched = "numeric" # Number of significant enriched pathways in down-regulated genes
+           up_enrichr = "tbl_df", # Enriched pathways in up-regulated genes
+           down_enrichr = "tbl_df", # Enriched pathways in down-regulated genes
+           sig_up_enrichr = "tbl_df", # Significant enriched pathways in up-regulated genes
+           sig_down_enrichr = "tbl_df", # Significant enriched pathways in down-regulated genes
+           num_upreg = "numeric", # Number of up-regulated genes
+           num_downreg = "numeric", # Number of down-regulated genes
+           num_up_enrichr = "numeric", # Number of enriched pathways in up-regulated genes
+           num_down_enrichr = "numeric", # Number of enriched pathways in down-regulated genes
+           num_sig_up_enrichr = "numeric", # Number of significant enriched pathways in up-regulated genes
+           num_sig_down_enrichr = "numeric" # Number of significant enriched pathways in down-regulated genes
          ))
 
+#' iLINCSResult-class: Class storing iLINCS Results
+#'
+#' This class stores the result of the results from iLINCS
+#'
+#' @slot threshold_up_ilincs numeric. LFC threshold for up-regulated genes
+#' @slot threshold_down_ilincs numeric. LFC threshold for down-regulated genes
+#' @slot threshold_pval_ilincs numeric. P Value threshold for genes.
+#' @slot threshold_similarity numeric. Similarity threshold
+#' @slot l1000_subset tbl_df. Subset of L1000 genes in the dataset
+#' @slot upregulated_ilincs tbl_df. L1000 genes up-regulatedd in dataset
+#' @slot downregulated_ilincs tbl_df. L1000 genes down-regulated in dataset
+#' @slot ilincs_query_signature tbl_df. Complete iLINCS Query signature
+#' @slot all_perturbagens_ilincs tbl_df. Complete list returned from iLINCS of perturbagens
+#' @slot filtered_perturbagens_ilincs tbl_df. Filtered list of perturbagens by the similarity threshold
+#' @slot num_genes_l1000 numeric. Number of L1000 genes in dataset
+#' @slot num_genes_upregulated_ilincs numeric. Number of up-regulated L1000 genes in dataset
+#' @slot num_genes_downregulated_ilincs numeric. Number of down-regulated L1000 genes in dataset
+#' @slot num_all_perturbagens_ilincs numeric. Number of all perturbagens returned by iLINCS
+#' @slot num_filtered_perturbagens_ilincs numeric. Number of filtered perturbagens returned by iLINCS
+#'
+#' @return
+#' @export
+#'
+#' @examples
 setClass("iLINCSResult",
          slots = c(
            threshold_up_ilincs = "numeric", # LFC threshold for up-regulated genes
@@ -75,6 +151,24 @@ setClass("IntegratedResult",
            placeholder = "numeric"
          ))
 
+#' BPNList-class: The BioPathNet Core Class
+#'
+#' This class takes care of the entire analysis infrastructure of the BioPathNet analysis.
+#' This class is created with inputting a list of differentially expressed genes and then
+#' stores the subsequently generated results.
+#'
+#' @slot input tbl_df. A tibble with Two columns of gene names and logfc values and optionally a p-value column
+#' @slot gsea GSEAResult. An instance of \link{GSEAResult-class} class storing the results GSEA Analysis
+#' @slot enrichr EnrichRResult. An instance of \link{EnrichRResult-class} class storing the results EnrichR Analysis
+#' @slot ilincs iLINCSResult. An instance of \link{iLINCSResult-class} class storing the results iLINCS Analysis
+#' @slot leading LEResult. An instance of \link{LEResult-class} class storing the results Leading Edge Analysis
+#' @slot revigo RevigoResult. An instance of \link{RevigoResult-class} class storing the results Revigo Analysis
+#' @slot integrated IntegratedResult. An instance of \link{IntegratedResult-class} class storing the results after integrating the three components
+#'
+#' @return
+#' @export
+#'
+#' @examples
 setClass("BPNList",
          slots = c(
            input = "tbl_df",
