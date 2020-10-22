2047f6a5d2d6fc5189a28e941cda468262a54a22
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-10-21 23:42:58 -0400

    Added a data file with documentation of the gmtdata

---
+++ R/data.R
@@ -0,0 +1,38 @@
+#' List of pathways and associated genes in homo sapiens
+#'
+#' The processed form of the October 2020 Pathway annotation
+#' containing every pathway annotated with the associated
+#' genes in humans. This uses the Symbol annotation from BaderLab.
+#'
+#'
+#' @format A list
+#'
+#' @source \url{http://download.baderlab.org/EM_Genesets/October_01_2020/Human/symbol/Human_GO_AllPathways_with_GO_iea_October_01_2020_symbol.gmt}
+
+"hsapiens"
+
+#' List of pathways and associated genes in mus musculus
+#'
+#' The processed form of the October 2020 Pathway annotation
+#' containing every pathway annotated with the associated
+#' genes in mice. This uses the Symbol annotation from BaderLab.
+#'
+#'
+#' @format A list
+#'
+#' @source \url{http://download.baderlab.org/EM_Genesets/October_01_2020/Mouse/symbol/Mouse_GO_AllPathways_with_GO_iea_October_01_2020_symbol.gmt}
+
+"mmusculus"
+
+#' List of pathways and associated genes in rattus norvegicus
+#'
+#' The processed form of the October 2020 Pathway annotation
+#' containing every pathway annotated with the associated
+#' genes in rats. This uses the Symbol annotation from BaderLab.
+#'
+#'
+#' @format A list
+#'
+#' @source \url{http://download.baderlab.org/EM_Genesets/October_01_2020/MRat/symbol/Rat_GO_AllPathways_with_GO_iea_October_01_2020_symbol.gmt}
+
+"rnorvegicus"
